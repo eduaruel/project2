3a80cdfd6500f8ef4cf0810c6891ca149aefa09a
Maykol <138355741+maykoldev@users.noreply.github.com>
2023-11-08 14:49:35 -0400

    actualizar5

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,4 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve('public/dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-		],
-	},
-};
+	}}
